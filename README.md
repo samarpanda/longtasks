b30b8637d522884e15105c8c559184a7fc1cf0c8
Samar Panda <er.samarpanda@gmail.com>
2019-01-17 19:31:16 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## LongTasks
+[longtasks](https://www.w3.org/TR/longtasks/) to capture long tasks occurrences in browser applications
+
+## Usage
+
+```js
+perfMetrics.onLongTask(function(entry){
+  console.log(`
+    Name: ${entry.name},
+    Type: ${entry.entryType},
+    Start: ${entry.startTime},
+    Duration: ${entry.duration},
+    ContainerType: ${entry.attribution.containerType},
+    AttributionName: ${entry.attribution[0].name},
+    ContainerType: ${entry.attribution[0].containerType},
+    ContainerId: ${entry.attribution[0].containerId || 'NA'},
+    ContainerName: ${entry.attribution[0].containerName || 'NA'},
+    ContainerSrc: ${entry.attribution[0].containerSrc || 'NA'}
+  `)
+  console.log(entry.attribution);
+})
+```
---
+++ src/longtasks.js
@@ -0,0 +1,47 @@
+(function(){
+
+  var LOAD_COMPLETE = 'complete';
+  var callbacks = [];
+  var listenerAdded = false;
+
+  function onLongTask(callback){
+    callbacks.push(callback)
+  }
+
+  function isLongTasksSupported(){
+    return 'PerformanceObserver' in window 
+      && 'PerformanceLongTaskTiming' in window 
+      && 'TaskAttributionTiming' in window
+  }
+
+  function subscribeToLongTasks(){
+    var observer = new PerformanceObserver(longTaskHandler)
+    observer.observe({entryTypes: ['longtask']})
+  }
+
+  function longTaskHandler(list){
+    list.getEntries().forEach(function(entry){
+      reportingLongTasks(entry)
+    })
+  }
+
+  function reportingLongTasks(entry){
+    callbacks.forEach(function(callback){
+      callback(entry)
+    })
+  }
+
+  if(isLongTasksSupported()){
+    if(document.readyState !== undefined && document.readyState === LOAD_COMPLETE){
+      subscribeToLongTasks()
+    } else {
+      listenerAdded = true
+      window.addEventListener('load', subscribeToLongTasks)
+    }
+  } else {
+    console.log(`LongTask not supported`)
+  }
+
+  self['perfMetrics'] = self['perfMetrics'] || {}
+  self['perfMetrics']['onLongTask'] = onLongTask
+}())
